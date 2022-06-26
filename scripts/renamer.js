2e5541a58127e919f01597e9c76600698d827718
Francois <venter109@gmail.com>
2022-06-26 14:21:55 +0200

    WIP trying to match some anime names

--- scripts/renamer.js
+++ scripts/renamer.js
@@ -24,67 +24,111 @@ basicRename = (files) => {
 getRenamedFile = async (file) => {
 	const extension = file.name.split(".").pop();
 	const basePath = file.path.replace(file.name, "");
+	const preppedName = file.name
+		.replace(/\s-\s/g, ".")
+		.replace(/\s\[[A-Za-z0-9\s]+\]\s/g, "");
 
-	/* 
-		TODO: Make this more generic to cater for more types of series naming 
-	*/
-	const startIndex = file.name.toLowerCase().search(/s\d+e\d+/);
+	// Matches something [Series Name s01e01 Bla]
+	const startIndexsDDeDD = file.name.toLowerCase().search(/s\d+e\d+/);
+	if (startIndexsDDeDD > 0) {
+		const seasonInfo = file.name
+			.substring(startIndex, file.name.length)
+			.split(".")
+			.shift()
+			.split(" ")
+			.shift();
 
-	// @TODO: Clean file name before processing
-	const preppedName = file.name.replace(/\s-\s/g, ".");
+		const seriesName = preppedName
+			.split(seasonInfo)
+			.shift()
+			.slice(0, -1)
+			.split(".")
+			.map((word) => {
+				return word[0].toUpperCase() + word.substring(1);
+			})
+			.join(" ")
+			.split(seasonInfo)[0]
+			.replace(/([0-9]{4})|(\[[A-Za-z]{1,}\])/, "")
+			.replace(/\s-/, "")
+			.trim();
 
-	const seasonInfo = file.name
-		.substring(startIndex, file.name.length)
-		.split(".")
-		.shift()
-		.split(" ")
-		.shift();
+		const seriesDetails = await axios
+			.get(`${tvMazeBaseUrl}/singlesearch/shows?q=${seriesName}`)
+			.then((res) => {
+				return res;
+			});
 
-	const seriesName = preppedName
-		.split(seasonInfo)
-		.shift()
-		.slice(0, -1)
-		.split(".")
-		.map((word) => {
-			return word[0].toUpperCase() + word.substring(1);
-		})
-		.join(" ")
-		.split(seasonInfo)[0]
-		.replace(/([0-9]{4})|(\[[A-Za-z]{1,}\])/, "")
-		.replace(/\s-/, "")
-		.trim();
+		const seasonNumber = seasonInfo
+			.toUpperCase()
+			.split("E")[0]
+			.split("")
+			.slice(1, seasonInfo.length - 2)
+			.join("");
 
-	const seriesDetails = await axios
-		.get(`${tvMazeBaseUrl}/singlesearch/shows?q=${seriesName}`)
-		.then((res) => {
-			return res;
-		});
+		const episodeNumber = seasonInfo
+			.toUpperCase()
+			.split("E")[1]
+			.split("V")
+			.shift();
 
-	const seasonNumber = seasonInfo
-		.toUpperCase()
-		.split("E")[0]
-		.split("")
-		.slice(1, seasonInfo.length - 2)
-		.join("");
+		const episodeDetails = await axios
+			.get(
+				`${tvMazeBaseUrl}/shows/${seriesDetails.data.id}/episodebynumber?season=${seasonNumber}&number=${episodeNumber}`
+			)
+			.then((res) => {
+				return res;
+			});
 
-	const episodeNumber = seasonInfo
-		.toUpperCase()
-		.split("E")[1]
-		.split("V")
-		.shift();
+		const cleanedSeasonInfo = `S${seasonNumber}E${episodeNumber}`;
+		const windowsFriendlyEpisodeName = episodeDetails.data.name
+			.replace(/\?|<|>|:|\*|\\|\/|\|/g, "")
+			.replace(/"/g, "'");
 
-	const episodeDetails = await axios
-		.get(
-			`${tvMazeBaseUrl}/shows/${seriesDetails.data.id}/episodebynumber?season=${seasonNumber}&number=${episodeNumber}`
-		)
-		.then((res) => {
-			return res;
-		});
+		return `${basePath}${seriesName}.${cleanedSeasonInfo}.${windowsFriendlyEpisodeName}.${extension}`;
+	}
 
-	const cleanedSeasonInfo = `S${seasonNumber}E${episodeNumber}`;
-	const windowsFriendlyEpisodeName = episodeDetails.data.name
-		.replace(/\?|<|>|:|\*|\\|\/|\|/g, "")
-		.replace(/"/g, "'");
+	// Matches something [Series Name 001 Bla]
+	const startIndexDigitsOnly = file.name.toLowerCase().search(/\d+/);
+	// if (startIndexDigitsOnly > 0) {
+	// 	const seriesName = preppedName.split(".")[0];
+	// 	let seriesDetails = await axios
+	// 		.get(`${tvMazeBaseUrl}/search/shows?q=${seriesName}`)
+	// 		.then((res) => {
+	// 			return res;
+	// 		});
 
-	return `${basePath}${seriesName}.${cleanedSeasonInfo}.${windowsFriendlyEpisodeName}.${extension}`;
+	// 	let tempSeriesName = seriesName;
+	// 	while (seriesDetails.data.length <= 0 && tempSeriesName.length) {
+	// 		console.log(tempSeriesName);
+	// 		seriesDetails = await axios
+	// 			.get(`${tvMazeBaseUrl}/search/shows?q=${tempSeriesName}`)
+	// 			.then((res) => {
+	// 				return res;
+	// 			});
+	// 		tempSeriesName = tempSeriesName.split(" ").slice(1, -1).join(" ");
+	// 	}
+
+	// 	const englishName = seriesDetails.data
+	// 		.pop()
+	// 		.show.name.toLowerCase()
+	// 		.replaceAll(" ", "-");
+
+	// 	console.log(seriesDetails);
+
+	// 	const episodeDetails = await axios
+	// 		.get(
+	// 			`${tvMazeBaseUrl}/shows/${
+	// 				seriesDetails.data.pop().show.id
+	// 			}/${englishName}/seasons`
+	// 		)
+	// 		.then((res) => {
+	// 			return res;
+	// 		});
+
+	// 	console.log(episodeDetails);
+	// }
+
+	return file.path;
 };
+
+getsDDeDD = (file) => {};
