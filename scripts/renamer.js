4f781a7cd2df1b798feab94a34488d64306ebd62
Francois <venter109@gmail.com>
2022-08-29 18:38:46 +0200

    added ddxdd functionality

--- scripts/renamer.js
+++ scripts/renamer.js
@@ -39,19 +39,22 @@ getRenamedFile = async (file) => {
 			.split(" ")
 			.shift();
 
-		let seriesName = preppedName
-			.split(seasonInfo)
-			.shift()
-			.slice(0, -1)
-			.split(".")
-			.map((word) => {
-				return word[0].toUpperCase() + word.substring(1);
-			})
-			.join(" ")
-			.split(seasonInfo)[0]
-			.replace(/([0-9]{4})|(\[[A-Za-z]{1,}\])/, "")
-			.replace(/\s-/, "")
-			.trim();
+		const seriesName =
+			customName.length > 0
+				? customName
+				: preppedName
+						.split(seasonInfo)
+						.shift()
+						.slice(0, -1)
+						.split(".")
+						.map((word) => {
+							return word[0].toUpperCase() + word.substring(1);
+						})
+						.join(" ")
+						.split(seasonInfo)[0]
+						.replace(/([0-9]{4})|(\[[A-Za-z]{1,}\])/, "")
+						.replace(/\s-/, "")
+						.trim();
 
 		const seriesDetails = await axios
 			.get(`${tvMazeBaseUrl}/singlesearch/shows?q=${seriesName}`)
@@ -88,48 +91,56 @@ getRenamedFile = async (file) => {
 		return `${basePath}${seriesName}.${cleanedSeasonInfo}.${windowsFriendlyEpisodeName}.${extension}`;
 	}
 
-	// Matches something [Series Name 001 Bla]
+	const startIndexDDxDD = file.name.toLowerCase().search(/\d+x\d+/);
+	if (startIndexDDxDD) {
+		const seriesName =
+			customName.length > 0
+				? customName
+				: file.name
+						.substring(0, startIndexDDxDD)
+						.replace(/-/g, "")
+						.replace(/"/g, "'")
+						.trim();
+
+		const seasonInfo = file.name
+			.substring(startIndexDDxDD, file.name.length)
+			.split(" ")[0];
+
+		const seriesDetails = await axios
+			.get(`${tvMazeBaseUrl}/singlesearch/shows?q=${seriesName}`)
+			.then((res) => {
+				return res;
+			});
+
+		const seasonNumber = seasonInfo
+			.toUpperCase()
+			.split("X")[0]
+			.split("")
+			.join("");
+
+		const episodeNumber = seasonInfo
+			.toUpperCase()
+			.split("X")[1]
+			.split("V")
+			.shift();
+
+		const episodeDetails = await axios
+			.get(
+				`${tvMazeBaseUrl}/shows/${seriesDetails.data.id}/episodebynumber?season=${seasonNumber}&number=${episodeNumber}`
+			)
+			.then((res) => {
+				return res;
+			});
+
+		const cleanedSeasonInfo = `S${seasonNumber}E${episodeNumber}`;
+		const windowsFriendlyEpisodeName = episodeDetails.data.name
+			.replace(/\?|<|>|:|\*|\\|\/|\|/g, "")
+			.replace(/"/g, "'");
+
+		return `${basePath}${seriesName}.${cleanedSeasonInfo}.${windowsFriendlyEpisodeName}.${extension}`;
+	}
+
 	const startIndexDigitsOnly = file.name.toLowerCase().search(/\d+/);
-	// if (startIndexDigitsOnly > 0) {
-	// 	const seriesName = preppedName.split(".")[0];
-	// 	let seriesDetails = await axios
-	// 		.get(`${tvMazeBaseUrl}/search/shows?q=${seriesName}`)
-	// 		.then((res) => {
-	// 			return res;
-	// 		});
 
-	// 	let tempSeriesName = seriesName;
-	// 	while (seriesDetails.data.length <= 0 && tempSeriesName.length) {
-	// 		console.log(tempSeriesName);
-	// 		seriesDetails = await axios
-	// 			.get(`${tvMazeBaseUrl}/search/shows?q=${tempSeriesName}`)
-	// 			.then((res) => {
-	// 				return res;
-	// 			});
-	// 		tempSeriesName = tempSeriesName.split(" ").slice(1, -1).join(" ");
-	// 	}
-
-	// 	const englishName = seriesDetails.data
-	// 		.pop()
-	// 		.show.name.toLowerCase()
-	// 		.replaceAll(" ", "-");
-
-	// 	console.log(seriesDetails);
-
-	// 	const episodeDetails = await axios
-	// 		.get(
-	// 			`${tvMazeBaseUrl}/shows/${
-	// 				seriesDetails.data.pop().show.id
-	// 			}/${englishName}/seasons`
-	// 		)
-	// 		.then((res) => {
-	// 			return res;
-	// 		});
-
-	// 	console.log(episodeDetails);
-	// }
-
-	return file.path;
+	// return file.path;
 };
-
-getsDDeDD = (file) => {};
